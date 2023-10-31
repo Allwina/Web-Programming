8d822fb5e4de05a5cd388aa2a7e14154b82c98a2
Allwina <85947960+Allwina@users.noreply.github.com>
2023-10-31 10:02:49 +0530

    Add files via upload

---
+++ date.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Date</title>
+</head>
+<body>
+    <?php
+    echo"Today is:";
+    echo date("Y/m/d") ."<br>";
+    echo date("y-m-d")."<br>";
+    echo date("Y.m.d");
+ 
+        ?>
+    
+</body>
+</html>
---
+++ pie.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    define("pi",3.14);
+    echo pi;
+    $r =5;
+    echo "area of circle is = " . (pi * $r * $r) . "<br />";  
+    ?>
+    
+</body>
+</html>
